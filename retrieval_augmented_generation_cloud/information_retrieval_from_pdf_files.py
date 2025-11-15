107ea9aa5370aa82c256393e82d835fafd28434b
Anil Kunwar <anilkunwar318@yahoo.com>
2025-11-15 18:57:32 +0100

    Update information_retrieval_from_pdf_files.py

--- retrieval_augmented_generation_cloud/information_retrieval_from_pdf_files.py
+++ retrieval_augmented_generation_cloud/information_retrieval_from_pdf_files.py
@@ -10,7 +10,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.llms import HuggingFaceHub
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
 # --- Configuration ---
